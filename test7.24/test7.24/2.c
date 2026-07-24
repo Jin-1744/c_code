b967969a948295d87b09578c9517c21b400d18c6
17 <qiushiqi26@163.com>
2026-07-24 22:45:00 +0800

    数位计算

---
+++ test7.24/test7.24/2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a = 40;
+	int c = 212;
+	printf(" % d", (-8 + 22) * a - 10 + c / 2);
+
+
+	return 0;
+}
---
+++ test7.24_2/test7.24_2/2.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main()
+{
+	int x;
+	int n = 1;
+	scanf("%d",&x);
+	while (x / 10 >= 1)
+	{
+		x = x / 10;
+		n++;
+
+	}
+	printf("%d",n);
+
+	return 0;
+}
