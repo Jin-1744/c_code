97af81e0c959d64d3677ec3bec332c0fd01b471c
17 <qiushiqi26@163.com>
2026-07-27 21:35:29 +0800

    if语句

---
+++ 7.26/if语句.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*int a = 4;
+int b = 6;
+int c = 6;
+int max;
+int main()
+{
+	if (a > b)
+	{
+		if (a > c)
+		{
+			max = a;
+		}
+		else
+		{
+			max = c;
+		}
+	}
+	else
+	{
+		if (c > b)
+		{
+			max = c;
+		}
+		else
+		{
+			max = b;
+		}
+	}
+	printf("%d\n", max);
+	return 0;
+}
+
+int main()
+
+{
+	int f;
+	int x;
+	printf("请输入一个整数：");
+	scanf("%d", &x);
+	if(x>10)
+	{
+		f = 1;
+	}
+	else if(x>0)
+	{
+		f = 0;
+	}
+	else
+	{
+		f = -1;
+	}
+	printf("%d\n", f);
+	return 0;
+}*/
+
